602a59539ace80abd9f22b8ae36b6340798f1e07
baohuyai <151175691+baohuyai@users.noreply.github.com>
2024-06-01 17:40:48 +0700

    Update tele_timefactory.lua

--- tele_timefactory.lua
+++ tele_timefactory.lua
@@ -12,7 +12,7 @@ if player then
         local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 
         -- Xác định tọa độ đích
-        local targetPosition = Vector3.new(x, y, z)  -- Thay đổi tọa độ đích theo yêu cầu của bạn
+        local targetPosition = Vector3.new(529.7452392578125, -56.917850494384766, -916.18798828125)  -- Thay đổi tọa độ đích theo yêu cầu của bạn
 
         -- Dịch chuyển nhân vật đến tọa độ đích
         humanoidRootPart.CFrame = CFrame.new(targetPosition)
